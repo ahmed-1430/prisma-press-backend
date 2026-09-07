2458ff18e1ca42ecbcfb4aa25f991431b6bd8920
ahmed-1430 <ahmedtexting@gmail.com>
2026-09-08 03:31:12 +0600

    add: db connection check clg and init db

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import app from "./app";
+import { db as prisma } from "./prisma/db";
 
 const PORT = process.env.PORT || 3000;
 async function main() {
     try {
+        await prisma.orm.public.User.where({ id: -1 }).first();
+        console.log(`Database connected successfully`);
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
         });
